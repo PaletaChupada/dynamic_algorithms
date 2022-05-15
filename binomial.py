530d26e513afe27fe10650d322025c80fa14dca2
PaletaChupada <espinoza.bautista.daniel246@gmail.com>
2022-05-15 15:05:23 -0500

    last changes

--- binomial.py
+++ binomial.py
@@ -7,7 +7,7 @@ Autor: Espinoza Bautista Daniel
 # Importamos las librerias
 from time import time
 
-def binomialCheck(n, k, dp):
+def calBinomial(n, k, dp):
      
     # Si el valor esta en la tabla de busqueda se regresa el valor
     if dp[n][k] != -1:
@@ -24,26 +24,26 @@ def binomialCheck(n, k, dp):
         return dp[n][k]
      
     # Guardamos el valor en la tabla de busqueda y realizamos el corrimiento del arreglo para realizar la busqueda
-    dp[n][k] = (binomialCheck(n-1, k-1, dp) +
-                binomialCheck(n-1, k, dp))
+    dp[n][k] = (calBinomial(n-1, k-1, dp) +
+                calBinomial(n-1, k, dp))
                  
     # Regresamos el arreglo de la tabla de busqueda
     return dp[n][k]
 
 def binomial(n, k):
      
-    # Creamos una tabla de busqueda temporal 
+    # Creamos una tabla de busqueda temporal
     dp = [ [ -1 for y in range(k + 1) ]
                 for x in range(n + 1) ]
  
     # Realizamos el chequeo de la tabla para hacer la suma del coeficiente binomial
-    return binomialCheck(n, k, dp)
+    return calBinomial(n, k, dp)
  
 # Solicitamos al usuario el numero al que calcularemos el coeficiente
 n = int(input("Dame el valor del numero a calcular su coeficiente: "))
 
 # Solicitamos al usuario el coeficiente para calcularle al numero
-k = int(input("Dame el valor del coeficiente: "))
+k = int(input("Dame el valor del co eficiente: "))
 print("\n")
 
 # Inicializamos la variable para contar el tiempo de ejecucion
